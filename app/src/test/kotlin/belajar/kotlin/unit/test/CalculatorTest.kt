942e8301c781e73a80136d54ae388dfeb247cf7e
krossmanzs <radi.adi78@gmail.com>
2021-12-27 00:50:17 +0700

    Sebelum dan sesudah test

--- app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
@@ -7,11 +7,38 @@ import org.junit.jupiter.api.Assertions.assertEquals
 @DisplayNameGeneration(SimpleDisplayNameGenerator::class)
 //@DisplayName("Test for Calculator class")
 class CalculatorTest {
+
+    companion object {
+
+        @BeforeAll
+        @JvmStatic
+        fun beforeALl() {
+            println("Sebelum semua unit test")
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun afterAll() {
+            println("Setelah semua unit test")
+        }
+    }
+
     val calculator = Calculator()
+
+    @BeforeEach
+    fun beforeEach() {
+        println("Sebelum Unit Test")
+    }
+
+    @AfterEach
+    fun afterEach() {
+        println("Setelah Unit Test")
+    }
 
     @Test
 //    @DisplayName("Test for function Calculator.add(10,10)")
     fun testAddSuccess1() {
+        println("Unit Test testAddSuccess1")
         val result = calculator.add(10, 10)
         assertEquals(20, result, "Hasil harus 20")
     }
@@ -19,6 +46,7 @@ class CalculatorTest {
     @Test
 //    @DisplayName("Test for function Calculator.add(15,10)")
     fun testAddSuccess2() {
+        println("Unit Test testAddSuccess2")
         val result = calculator.add(15, 10)
         assertEquals(25, result, "Hasil harus 25")
     }
@@ -26,6 +54,7 @@ class CalculatorTest {
     @Test
 //    @DisplayName("Test for function Calculator.divide(100,10)")
     fun testDivideSuccess() {
+        println("Unit Test testDivideSuccess")
         val result = calculator.divide(100, 10)
         assertEquals(10.0f, result)
     }
@@ -33,6 +62,7 @@ class CalculatorTest {
     @Test
 //    @DisplayName("Test error when divide by zero on function Calculator.divide(100,0)")
     fun testDivideError() {
+        println("Unit Test testDivideError")
         assertThrows<IllegalArgumentException> {
             calculator.divide(100, 0)
         }
