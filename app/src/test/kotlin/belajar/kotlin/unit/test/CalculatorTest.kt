85808a417a58058998163be929ff6f116f943e7e
krossmanzs <radi.adi78@gmail.com>
2021-12-27 00:30:06 +0700

    Mengubah nama Test menggunakan DisplayNameGenerator Interface

--- app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
@@ -1,31 +1,40 @@
 package belajar.kotlin.unit.test
 
+import belajar.kotlin.unit.test.generator.SimpleDisplayNameGenerator
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.assertThrows
 
+@DisplayNameGeneration(SimpleDisplayNameGenerator::class)
+//@DisplayName("Test for Calculator class")
 class CalculatorTest {
     val calculator = Calculator()
 
     @Test
+//    @DisplayName("Test for function Calculator.add(10,10)")
     fun testAddSuccess1() {
         val result = calculator.add(10, 10)
         assertEquals(20, result, "Hasil harus 20")
     }
 
     @Test
+//    @DisplayName("Test for function Calculator.add(15,10)")
     fun testAddSuccess2() {
         val result = calculator.add(15, 10)
         assertEquals(25, result, "Hasil harus 25")
     }
 
     @Test
+//    @DisplayName("Test for function Calculator.divide(100,10)")
     fun testDivideSuccess() {
         val result = calculator.divide(100, 10)
         assertEquals(10.0f, result)
     }
 
     @Test
+//    @DisplayName("Test error when divide by zero on function Calculator.divide(100,0)")
     fun testDivideError() {
         assertThrows<IllegalArgumentException> {
             calculator.divide(100, 0)
