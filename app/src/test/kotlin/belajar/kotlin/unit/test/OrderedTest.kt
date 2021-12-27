6b12313d47b815164ca3a1b23e4e866d9d96f03f
krossmanzs <radi.adi78@gmail.com>
2021-12-27 14:55:25 +0700

    Siklus hidup test menggunakan TestInstance

--- app/src/test/kotlin/belajar/kotlin/unit/test/OrderedTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/OrderedTest.kt
@@ -1,28 +1,47 @@
 package belajar.kotlin.unit.test
 
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.*
 
+//@TestInstance(value = TestInstance.Lifecycle.PER_METHOD) // DEFAULT
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation::class)
 class OrderedTest {
+
+    var counter: Int = 0
+
+    /**
+     * Ketika menggunakan TestInstance PER_CLASS maka kita
+     * bisa melakukan before after test tanpa membuat companion object
+     */
+
+    @BeforeAll
+    fun beforeAll() {
+        println("Sebelum semua test")
+    }
+
+    @AfterAll
+    fun afterAll() {
+        println("Sesudah semua test")
+    }
 
     @Test
     @Order(2)
     fun test1() {
-
+        counter++
+        println(counter)
     }
 
     @Test
     @Order(3)
     fun test2() {
-
+        counter++
+        println(counter)
     }
 
     @Test
     @Order(1)
     fun test3() {
-
+        counter++
+        println(counter)
     }
 }
