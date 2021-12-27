b454080afdbe28b6cb43e9fe864bb67978aba54b
krossmanzs <radi.adi78@gmail.com>
2021-12-27 15:43:43 +0700

    Test berulang

--- app/src/test/kotlin/belajar/kotlin/unit/test/RandomCalculatorTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/RandomCalculatorTest.kt
@@ -1,9 +1,8 @@
 package belajar.kotlin.unit.test
 
 import belajar.kotlin.unit.test.resolver.RandomParameterResolver
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.TestInfo
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.Extensions
 import java.util.*
@@ -23,6 +22,26 @@ class RandomCalculatorTest : ParentCalculatorTest() {
 
         println("""
             $first + $second = ${first + second}
+        """.trimIndent())
+
+        val result = calculator.add(first, second)
+
+        assertEquals(first + second, result)
+    }
+
+    @DisplayName("Test calculator random")
+    @RepeatedTest(
+        value = 10,
+        name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    fun testRandomRepeated(random: Random, testInfo: TestInfo) {
+        val first = random.nextInt(1000)
+        val second = random.nextInt(1000)
+
+        println("""
+            ${testInfo.displayName}
+            $first + $second = ${first + second}
+            
         """.trimIndent())
 
         val result = calculator.add(first, second)
