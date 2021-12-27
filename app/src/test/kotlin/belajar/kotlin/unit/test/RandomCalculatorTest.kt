f01f5bc2b87daa1b90d104db03cb9fef17be7db6
krossmanzs <radi.adi78@gmail.com>
2021-12-27 15:21:00 +0700

    Menggunakan nilai ekspektasi yang berubah ubah menggunakan dependency injection pada Unit Test

---
+++ app/src/test/kotlin/belajar/kotlin/unit/test/RandomCalculatorTest.kt
@@ -0,0 +1,32 @@
+package belajar.kotlin.unit.test
+
+import belajar.kotlin.unit.test.resolver.RandomParameterResolver
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.TestInfo
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.Extensions
+import java.util.*
+
+@Extensions(value = [
+    ExtendWith(RandomParameterResolver::class)
+])
+class RandomCalculatorTest {
+
+    private val calculator = Calculator()
+
+    @Test
+    fun testRandom(random: Random) {
+        val first = random.nextInt(1000)
+        val second = random.nextInt(1000)
+
+        println("""
+            $first + $second = ${first + second}
+        """.trimIndent())
+
+        val result = calculator.add(first, second)
+
+        assertEquals(first + second, result)
+    }
+
+}
